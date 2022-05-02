613c6918cefedadda6d8969ace519a0d4557dc82
Сергей Ивеснкий <ivenskijsv@gmail.com>
2022-05-03 01:20:03 +0300

    Reformat Code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,9 +1,9 @@
 public class BmiService {
 
-    public double calculate (int bodyMass, double growth) {
+    public double calculate(int bodyMass, double growth) {
         double degree = growth * growth;
         double bmi = bodyMass / degree;
 
-        return (double) bmi;
+        return bmi;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        int bodyMass = 110;
-        double growth = 1.87;
-        int bmi = (int) service.calculate(bodyMass,growth);
+        int bodyMass = 88;
+        double growth = 1.85;
+        int bmi = (int) service.calculate(bodyMass, growth);
 
         System.out.println(bmi);
     }
